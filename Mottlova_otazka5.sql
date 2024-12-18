186182454be11c3c8f6961683f19ae29d94dda91
Andrea Mottlova <mottlova.andrea1@gmail.com>
2024-12-18 22:25:45 +0100

    prejmenovani sloupcu

--- Mottlova_otazka5.sql
+++ Mottlova_otazka5.sql
@@ -1,32 +1,39 @@
+-- Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to na cenách 
+-- potravin či mzdách ve stejném nebo násdujícím roce výraznějším růstem?
 
+
+-- tabulka vše
+SELECT * 
+FROM t_andrea_mottlova_project_sql_primary_final AS tampspf 
+
+---------
 
 SELECT rok  
-	   , round(HDP/1000000,2) AS 'HDP_v_mil'
-	   , round( (lag(HDP) OVER (ORDER BY rok))/1000000,2) AS 'HDP_predch_rok_v_mil'
+	   , round(HDP/1000000) AS 'HDP_v_mil'
+	   , round( (lag(HDP) OVER (ORDER BY rok))/1000000) AS 'HDP_predch_rok_v_mil'
 	   , round( (HDP/lag(HDP) OVER (ORDER BY rok) *100)-100,2) AS 'HDP_rozdil_procenta'
-	   , round(avg(cena_cena),2) AS prumerna_cena
-	   , round(lag(avg(cena_cena)) OVER (ORDER BY rok),2) AS cena_predch_rok
-	   , round( (avg(cena_cena)/lag(avg(cena_cena)) OVER (ORDER BY rok) *100)-100,2) AS 'cena_rozdil_procenta'
-	   , round(avg(zamest_hodnota),2) AS prumerna_mzda
-	   , round(lag(avg(zamest_hodnota)) OVER (ORDER BY rok),2) AS mzda_predch_rok
-	   , round( (avg(zamest_hodnota)/lag(avg(zamest_hodnota)) OVER (ORDER BY rok) *100)-100,2) AS 'mzda_rozdil_procenta'
+	   , round(avg(cena),2) AS prumerna_cena
+	   , round(lag(avg(cena)) OVER (ORDER BY rok),2) AS cena_predch_rok
+	   , round( (avg(cena)/lag(avg(cena)) OVER (ORDER BY rok) *100)-100,2) AS 'cena_rozdil_procenta'
+	   , round(avg(prumerna_mzda),2) AS prumerna_mzda
+	   , round(lag(avg(prumerna_mzda)) OVER (ORDER BY rok),2) AS mzda_predch_rok
+	   , round( (avg(prumerna_mzda)/lag(avg(prumerna_mzda)) OVER (ORDER BY rok) *100)-100,2) AS 'mzda_rozdil_procenta'
 	   ,CASE 
 	  	WHEN (HDP/lag(HDP) OVER (ORDER BY rok)) *100-100 >0 THEN 'růst'
 	 	WHEN (HDP/lag(HDP) OVER (ORDER BY rok)) *100-100 <0 THEN 'pokles'
 	 	WHEN (HDP/lag(HDP) OVER (ORDER BY rok)) *100-100 =0 THEN 'stagnace'
 	    END AS 'HDP_vývoj'
 	   ,CASE 
-	  	WHEN (avg(cena_cena)/lag(avg(cena_cena)) OVER (ORDER BY rok) *100)-100 >0 THEN 'růst'
-	 	WHEN (avg(cena_cena)/lag(avg(cena_cena)) OVER (ORDER BY rok) *100)-100 <0 THEN 'pokles'
-	 	WHEN (avg(cena_cena)/lag(avg(cena_cena)) OVER (ORDER BY rok) *100)-100 =0 THEN 'stagnace'
+	  	WHEN (avg(cena)/lag(avg(cena)) OVER (ORDER BY rok) *100)-100 >0 THEN 'růst'
+	 	WHEN (avg(cena)/lag(avg(cena)) OVER (ORDER BY rok) *100)-100 <0 THEN 'pokles'
+	 	WHEN (avg(cena)/lag(avg(cena)) OVER (ORDER BY rok) *100)-100 =0 THEN 'stagnace'
 	    END AS 'cena_vývoj'
 	    ,CASE 
-	  	WHEN (avg(zamest_hodnota)/lag(avg(zamest_hodnota)) OVER (ORDER BY rok) *100)-100 >0 THEN 'růst'
-	 	WHEN (avg(zamest_hodnota)/lag(avg(zamest_hodnota)) OVER (ORDER BY rok) *100)-100 <0 THEN 'pokles'
-	 	WHEN (avg(zamest_hodnota)/lag(avg(zamest_hodnota)) OVER (ORDER BY rok) *100)-100 =0 THEN 'stagnace'
+	  	WHEN (avg(prumerna_mzda)/lag(avg(prumerna_mzda)) OVER (ORDER BY rok) *100)-100 >0 THEN 'růst'
+	 	WHEN (avg(prumerna_mzda)/lag(avg(prumerna_mzda)) OVER (ORDER BY rok) *100)-100 <0 THEN 'pokles'
+	 	WHEN (avg(prumerna_mzda)/lag(avg(prumerna_mzda)) OVER (ORDER BY rok) *100)-100 =0 THEN 'stagnace'
 	    END AS 'mzdy_vývoj'
 FROM t_andrea_mottlova_project_sql_primary_final AS tampspf 
-WHERE zamest_kod_typ =5958
 GROUP BY rok, HDP
 
 
