618ae1907dfe878674b15744c0efe20d2b3f0ada
Andrea Mottlova <mottlova.andrea1@gmail.com>
2024-12-18 22:11:49 +0100

    prejmenovani sloupcu

--- Mottlova_otazka2.sql
+++ Mottlova_otazka2.sql
@@ -1,12 +1,21 @@
-SELECT rok 
-	   , cena_kategorie 
-	   , round(avg(cena_cena),2) AS prumerna_cena
-	   , round(avg(zamest_hodnota)) AS prumerna_mzda
-	   , round(avg(zamest_hodnota)/avg(cena_cena),2) AS pocet
+-- Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
+
+
+-- tabulka vše
+SELECT * 
 FROM t_andrea_mottlova_project_sql_primary_final AS tampspf 
-WHERE (cena_kategorie =  'Mléko polotučné pasterované' OR 
-	   cena_kategorie = 'Chléb konzumní kmínový') AND 
+
+---------
+
+SELECT rok 
+	   , kategorie_potraviny
+	   , round(avg(cena),2) AS prumerna_cena
+	   , round(avg(prumerna_mzda)) AS prumerna_mzda
+	   , floor(avg(prumerna_mzda)/avg(cena)) AS pocet
+FROM t_andrea_mottlova_project_sql_primary_final AS tampspf 
+WHERE (kategorie_potraviny=  'Mléko polotučné pasterované' OR 
+	   kategorie_potraviny= 'Chléb konzumní kmínový') AND 
 	   (rok = 2006 OR 
 	   rok = 2018)
-GROUP BY rok, cena_kategorie 
-ORDER BY cena_kategorie, rok 
+GROUP BY rok, kategorie_potraviny
+ORDER BY kategorie_potraviny, rok 
