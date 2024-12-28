ff95fdb3b373ed72fbec801de7c051ed3f29117e
Andrea Mottlova <mottlova.andrea1@gmail.com>
2024-12-28 21:24:08 +0100

    zmena

--- Mottlova_otazka3.sql
+++ Mottlova_otazka3.sql
@@ -1,14 +1,6 @@
 -- Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
 
 
--- tabulka vše
-SELECT * 
-FROM t_andrea_mottlova_project_sql_primary_final AS tampspf 
-
----------
-
-
-
 SELECT rok
 	   , kategorie_potraviny 
 	   , round(avg(cena),2) AS prumerna_cena
@@ -18,7 +10,7 @@ FROM t_andrea_mottlova_project_sql_primary_final AS tampspf
 GROUP BY kategorie_potraviny , rok
 
 
-
+-- --------------------------------------------------------------------------------------------- 
 -- porovnání nárustu cen za celé období (půměr + suma)
 WITH cte_am_zdrazovani_potravin AS (
 	SELECT rok
@@ -30,9 +22,9 @@ WITH cte_am_zdrazovani_potravin AS (
 	GROUP BY kategorie_potraviny , rok
 )
 SELECT kategorie_potraviny
-	   , round( avg(prum_cena_rozdil_procenta),2) AS prumerny_narust
-	   , sum(prum_cena_rozdil_procenta) AS suma_narust
+	   , round( avg(prum_cena_rozdil_procenta),2) AS prumerny_narust_procenta
+	   , sum(prum_cena_rozdil_procenta) AS suma_narust_procenta
 FROM cte_am_zdrazovani_potravin
 GROUP BY kategorie_potraviny
-ORDER BY suma_narust
+ORDER BY suma_narust_procenta
 
