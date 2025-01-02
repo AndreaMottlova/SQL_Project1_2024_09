792da82f79b921de01151cfdb21eb38f33127625
Andrea Mottlova <mottlova.andrea1@gmail.com>
2025-01-02 22:49:10 +0100

    připojeni secondary tabulky

--- Mottlova_otazka5.sql
+++ Mottlova_otazka5.sql
@@ -1,38 +1,34 @@
--- Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to na cenách 
+-- Má výška sec.hdp vliv na změny ve mzdách a cenách potravin? Neboli, pokud sec.hdp vzroste výrazněji v jednom roce, projeví se to na cenách 
 -- potravin či mzdách ve stejném nebo násdujícím roce výraznějším růstem?
 
 
--- tabulka vše
-SELECT * 
-FROM t_andrea_mottlova_project_sql_primary_final AS tampspf 
-
----------
-
-SELECT rok  
-	   , round(HDP/1000000) AS 'HDP_v_mil'
-	   , round( (lag(HDP) OVER (ORDER BY rok))/1000000) AS 'HDP_predch_rok_v_mil'
-	   , round( (HDP/lag(HDP) OVER (ORDER BY rok) *100)-100,2) AS 'HDP_rozdil_procenta'
-	   , round(avg(cena),2) AS prumerna_cena
-	   , round(lag(avg(cena)) OVER (ORDER BY rok),2) AS cena_predch_rok
-	   , round( (avg(cena)/lag(avg(cena)) OVER (ORDER BY rok) *100)-100,2) AS 'cena_rozdil_procenta'
-	   , round(avg(prumerna_mzda),2) AS prumerna_mzda
-	   , round(lag(avg(prumerna_mzda)) OVER (ORDER BY rok),2) AS mzda_predch_rok
-	   , round( (avg(prumerna_mzda)/lag(avg(prumerna_mzda)) OVER (ORDER BY rok) *100)-100,2) AS 'mzda_rozdil_procenta'
+SELECT prim.rok  
+	   , round(sec.hdp/1000000) AS hdp_v_mil
+	   , round( (lag(sec.hdp) OVER (ORDER BY prim.rok))/1000000) AS hdp_predch_rok_v_mil
+	   , round( (sec.hdp/lag(sec.hdp) OVER (ORDER BY prim.rok) *100)-100,2) AS hdp_rozdil_procenta
+	   , round(avg(prim.cena),2) AS prumerna_cena
+	   , round(lag(avg(prim.cena)) OVER (ORDER BY prim.rok),2) AS cena_predch_rok
+	   , round( (avg(prim.cena)/lag(avg(prim.cena)) OVER (ORDER BY prim.rok) *100)-100,2) AS cena_rozdil_procenta
+	   , round(avg(prim.prumerna_mzda),2) AS prumerna_mzda
+	   , round(lag(avg(prim.prumerna_mzda)) OVER (ORDER BY prim.rok),2) AS mzda_predch_rok
+	   , round( (avg(prim.prumerna_mzda)/lag(avg(prim.prumerna_mzda)) OVER (ORDER BY prim.rok) *100)-100,2) AS mzda_rozdil_procenta
 	   ,CASE 
-	  	WHEN (HDP/lag(HDP) OVER (ORDER BY rok)) *100-100 >0 THEN 'růst'
-	 	WHEN (HDP/lag(HDP) OVER (ORDER BY rok)) *100-100 <0 THEN 'pokles'
-	 	WHEN (HDP/lag(HDP) OVER (ORDER BY rok)) *100-100 =0 THEN 'stagnace'
-	    END AS 'HDP_vývoj'
+	  	WHEN (sec.hdp/lag(sec.hdp) OVER (ORDER BY prim.rok)) *100-100 >0 THEN 'růst'
+	 	WHEN (sec.hdp/lag(sec.hdp) OVER (ORDER BY prim.rok)) *100-100 <0 THEN 'pokles'
+	 	WHEN (sec.hdp/lag(sec.hdp) OVER (ORDER BY prim.rok)) *100-100 =0 THEN 'stagnace'
+	    END AS hdp_vývoj
 	   ,CASE 
-	  	WHEN (avg(cena)/lag(avg(cena)) OVER (ORDER BY rok) *100)-100 >0 THEN 'růst'
-	 	WHEN (avg(cena)/lag(avg(cena)) OVER (ORDER BY rok) *100)-100 <0 THEN 'pokles'
-	 	WHEN (avg(cena)/lag(avg(cena)) OVER (ORDER BY rok) *100)-100 =0 THEN 'stagnace'
-	    END AS 'cena_vývoj'
+	  	WHEN (avg(prim.cena)/lag(avg(prim.cena)) OVER (ORDER BY prim.rok) *100)-100 >0 THEN 'růst'
+	 	WHEN (avg(prim.cena)/lag(avg(prim.cena)) OVER (ORDER BY prim.rok) *100)-100 <0 THEN 'pokles'
+	 	WHEN (avg(prim.cena)/lag(avg(prim.cena)) OVER (ORDER BY prim.rok) *100)-100 =0 THEN 'stagnace'
+	    END AS cena_vývoj
 	    ,CASE 
-	  	WHEN (avg(prumerna_mzda)/lag(avg(prumerna_mzda)) OVER (ORDER BY rok) *100)-100 >0 THEN 'růst'
-	 	WHEN (avg(prumerna_mzda)/lag(avg(prumerna_mzda)) OVER (ORDER BY rok) *100)-100 <0 THEN 'pokles'
-	 	WHEN (avg(prumerna_mzda)/lag(avg(prumerna_mzda)) OVER (ORDER BY rok) *100)-100 =0 THEN 'stagnace'
-	    END AS 'mzdy_vývoj'
-FROM t_andrea_mottlova_project_sql_primary_final AS tampspf 
-GROUP BY rok, HDP
-
+	  	WHEN (avg(prim.prumerna_mzda)/lag(avg(prim.prumerna_mzda)) OVER (ORDER BY prim.rok) *100)-100 >0 THEN 'růst'
+	 	WHEN (avg(prim.prumerna_mzda)/lag(avg(prim.prumerna_mzda)) OVER (ORDER BY prim.rok) *100)-100 <0 THEN 'pokles'
+	 	WHEN (avg(prim.prumerna_mzda)/lag(avg(prim.prumerna_mzda)) OVER (ORDER BY prim.rok) *100)-100 =0 THEN 'stagnace'
+	    END AS mzdy_vývoj
+FROM t_andrea_mottlova_project_sql_primary_final AS prim
+JOIN t_andrea_mottlova_project_sql_secondary_final AS sec 
+	ON prim.rok=sec.rok 
+WHERE sec.stat = 'Czech Republic'
+GROUP BY prim.rok, sec.hdp
